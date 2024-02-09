9383ce751081fa366d982eaaf90b917143fb6828
nigamchitransh208 <nigamchitransh208@gmail.com>
2024-02-09 13:34:34 -0800

    Routing by Chitransh

--- kartavya/index.js
+++ kartavya/index.js
@@ -1,3 +1,34 @@
+class sHeader extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML=` <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="map.html">Map</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="../chitransh/index.html">Create Club</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>`
+    }
+}
+
+customElements.define('s-header',sHeader);
 // Initialize Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyC3L-pygyvZqYOGp5Os7swV54Mhno1To88",
