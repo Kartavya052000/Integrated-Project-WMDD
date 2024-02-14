9f5254b76fc231737f2f25c193d03483b38c6555
ARUN SINGH <arunsinghh0011@gmail.com>
2024-02-14 13:24:43 -0800

    added my profile page

--- js/index.js
+++ js/index.js
@@ -20,6 +20,9 @@ class sHeader extends HTMLElement {
               <li class="nav-item">
               <a class="nav-link" href="login.html">Login</a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link " href="myprofile.html">My Profile</a>
+              </li>
             </ul>
             <form class="d-flex" role="search">
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -33,39 +36,39 @@ class sHeader extends HTMLElement {
 
 customElements.define("s-header", sHeader);
 // Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyC3L-pygyvZqYOGp5Os7swV54Mhno1To88",
-  authDomain: "test-8e125.firebaseapp.com",
-  databaseURL: "https://test-8e125-default-rtdb.firebaseio.com",
-  projectId: "test-8e125",
-  storageBucket: "test-8e125.appspot.com",
-  messagingSenderId: "675271753145",
-  appId: "1:675271753145:web:0f2070f6b149b210608a68",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC3L-pygyvZqYOGp5Os7swV54Mhno1To88",
+//   authDomain: "test-8e125.firebaseapp.com",
+//   databaseURL: "https://test-8e125-default-rtdb.firebaseio.com",
+//   projectId: "test-8e125",
+//   storageBucket: "test-8e125.appspot.com",
+//   messagingSenderId: "675271753145",
+//   appId: "1:675271753145:web:0f2070f6b149b210608a68",
+// };
 
-firebase.initializeApp(firebaseConfig); // Initialize Firebase
+// firebase.initializeApp(firebaseConfig); // Initialize Firebase
 
-// Function to create a club
-async function createClub(userId, clubName, clubDescription) {
-  try {
-    // Add club document to "clubs" collection
-    const clubRef = await firebase.firestore().collection("clubs").add({
-      name: clubName,
-      description: clubDescription,
-      admin: userId,
-    });
+// // Function to create a club
+// async function createClub(userId, clubName, clubDescription) {
+//   try {
+//     // Add club document to "clubs" collection
+//     const clubRef = await firebase.firestore().collection("clubs").add({
+//       name: clubName,
+//       description: clubDescription,
+//       admin: userId,
+//     });
 
-    // Add user as admin in the "members" subcollection of the club
-    await clubRef.collection("members").doc(userId).set({
-      userId: userId,
-      role: "admin",
-    });
+//     // Add user as admin in the "members" subcollection of the club
+//     await clubRef.collection("members").doc(userId).set({
+//       userId: userId,
+//       role: "admin",
+//     });
 
-    console.log("Club created successfully!");
-  } catch (error) {
-    console.error("Error creating club: ", error);
-  }
-}
+//     console.log("Club created successfully!");
+//   } catch (error) {
+//     console.error("Error creating club: ", error);
+//   }
+// }
 
 // Call the createClub function
 // createClub("zAuTYTmuOcgGFjI0daHLLCXjZPX2", "My Club", "Club Description");
