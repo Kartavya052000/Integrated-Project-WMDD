fe3685c0afe0d71957de06b7be23b56bd1eb05a8
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-03-08 23:29:39 -0800

    structure assemble

--- js/index.js
+++ js/index.js
@@ -28,46 +28,37 @@ const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 class sHeader extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = ` <nav class="navbar bg-info navbar-expand-lg  bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">SportsCrush</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    this.innerHTML = `<nav class="navbar navbar-light" style="background-color: lightgreen;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.html">SportsCrush</a>
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link " aria-current="page" href="index.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="map.html">Map</a>
-              </li>
-              <li class="nav-item">
-              <a class="nav-link" href="create_club.html">Create Club</a>
-              </li>
-              <li class="nav-item">
-              <a class="nav-link" href="login.html">Login</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="myprofile.html">My Profile</a>
-              </li>
-              <li class="nav-item">
-              <a class="nav-link" id="myclubs" style="cursor: pointer;">My Clubs</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" id="manageclubs" style="cursor: pointer;">Manage Clubs</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" id="recommendations" style="cursor: pointer;">Recommendations</a>
-              </li>
+              
+                <li class="nav-item">
+                    <a class="nav-link" href="create_club.html">Create Club</a>
+                </li>
+               
+                <li class="nav-item">
+                    <a class="nav-link" href="myprofile.html">My Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="myclubs" style="cursor: pointer;">My Clubs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="manageclubs" style="cursor: pointer;">Manage Clubs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="recommendations" style="cursor: pointer;">Recommendations</a>
+                </li>
             </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
         </div>
-      </nav>`;
+    </div>
+</nav>
+`;
   }
 }
 
