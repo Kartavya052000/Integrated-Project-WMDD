aa9ddbecc7cc290c2907f04879b4a192ed066675
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-03-19 13:34:51 -0700

    checkout branch

--- js/login.js
+++ js/login.js
@@ -105,79 +105,79 @@ document.getElementById("login").addEventListener("click", function () {
 });
 
 // GoogleSignIn
-document.getElementById("googleSignIn").addEventListener("click", function () {
-  signInWithPopup(auth, provider)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log(user);
-      localStorage.setItem("user", JSON.stringify(user));
+// document.getElementById("googleSignIn").addEventListener("click", function () {
+//   signInWithPopup(auth, provider)
+//     .then((userCredential) => {
+//       const user = userCredential.user;
+//       console.log(user);
+//       localStorage.setItem("user", JSON.stringify(user));
 
-      const userInfo = {
-        uid: user.uid,
-        email: user.email,
-        displayName: user.displayName,
+//       const userInfo = {
+//         uid: user.uid,
+//         email: user.email,
+//         displayName: user.displayName,
 
-        // Add other user information as needed
-      };
+//         // Add other user information as needed
+//       };
 
-      // Firestore collection reference
-      const usersCollection = collection(firestore, "users");
+//       // Firestore collection reference
+//       const usersCollection = collection(firestore, "users");
 
-      setDoc(doc(usersCollection, user.uid), userInfo)
-        .then(() => {
-          alert("Welcome, " + user.displayName);
-        })
-        .catch((error) => {
-          alert("Error saving user information: " + error.message);
-        });
-    })
-    .catch((error) => {
-      console.error("Google sign-in error:", error);
-      alert("Google sign-in error: " + error.message);
-    });
-});
+//       setDoc(doc(usersCollection, user.uid), userInfo)
+//         .then(() => {
+//           alert("Welcome, " + user.displayName);
+//         })
+//         .catch((error) => {
+//           alert("Error saving user information: " + error.message);
+//         });
+//     })
+//     .catch((error) => {
+//       console.error("Google sign-in error:", error);
+//       alert("Google sign-in error: " + error.message);
+//     });
+// });
 
 // Sign-out
-document.getElementById("logout").addEventListener("click", function () {
-  signOut(auth)
-    .then(() => {
-      localStorage.removeItem("user");
-      alert("logout");
-      window.location.reload();
-    })
-    .catch((error) => {
-      alert("logout error:", error);
-    });
-});
+// document.getElementById("logout").addEventListener("click", function () {
+//   signOut(auth)
+//     .then(() => {
+//       localStorage.removeItem("user");
+//       alert("logout");
+//       window.location.reload();
+//     })
+//     .catch((error) => {
+//       alert("logout error:", error);
+//     });
+// });
 
 const user = JSON.parse(localStorage.getItem("user"));
-if (user) {
-  document.getElementById("loginContainer").style.display = "none";
-  document.getElementById("userDetailsWrapper").style.display = "block";
+// if (user) {
+//   document.getElementById("loginContainer").style.display = "none";
+//   document.getElementById("userDetailsWrapper").style.display = "block";
 
-  const userDetails = document.getElementById("userDetails");
-  userDetails.innerHTML = `
-                    <div style="text-align:center">
-                        <h2>User:${user.email}</h2>
-                    `;
-} else {
-  document.getElementById("loginContainer").style.display = "block";
-  document.getElementById("userDetailsWrapper").style.display = "none";
-}
+//   const userDetails = document.getElementById("userDetails");
+//   userDetails.innerHTML = `
+//                     <div style="text-align:center">
+//                         <h2>User:${user.email}</h2>
+//                     `;
+// } else {
+//   document.getElementById("loginContainer").style.display = "block";
+//   document.getElementById("userDetailsWrapper").style.display = "none";
+// }
 
 
 // for hide show password
-const passwordField = document.getElementById("password");
-const togglePassword = document.querySelector(".password-toggle-icon i");
+// const passwordField = document.getElementById("password");
+// const togglePassword = document.querySelector(".password-toggle-icon i");
 
-togglePassword.addEventListener("click", function () {
-  if (passwordField.type === "password") {
-    passwordField.type = "text";
-    togglePassword.classList.remove("fa-eye");
-    togglePassword.classList.add("fa-eye-slash");
-  } else {
-    passwordField.type = "password";
-    togglePassword.classList.remove("fa-eye-slash");
-    togglePassword.classList.add("fa-eye");
-  }
-});
+// togglePassword.addEventListener("click", function () {
+//   if (passwordField.type === "password") {
+//     passwordField.type = "text";
+//     togglePassword.classList.remove("fa-eye");
+//     togglePassword.classList.add("fa-eye-slash");
+//   } else {
+//     passwordField.type = "password";
+//     togglePassword.classList.remove("fa-eye-slash");
+//     togglePassword.classList.add("fa-eye");
+//   }
+// });
